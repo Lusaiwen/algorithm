5dd6a17ee016879a032e144505550a33619fb654
Lusaiwen <1811977648@qq.com>
2021-01-24 21:58:37 +0800

    5.简单快速排序

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 
-
 var arr = [4,7,8,3,2,1,5,5,9,6];
 
 
-//arr.sort(fucntion(){return a - b > 0})
-function compare(a, b){
-  return a - b > 0;
-}
-
-function exchange(arr, a, b){
-  var temp = arr[a];
-  arr[a] = arr[b];
-  arr[b] = temp;
-}
-
-
-function sort(arr){
-  for(var i = 0; i< arr.length; i++){
-    var maxIndex = 0;
-    for(var j = 0; j < arr.length - i; j ++){
-      if(compare(arr[j], arr[maxIndex])){
-        maxIndex = j;
-      }
-    }
-    exchange(arr, maxIndex, arr.length - 1 - i);
+//简单快排，消耗性能，易于理解
+function quickSort(arr){
+  if(arr == null || arr.length == 0) return [];
+  //1. 选班长
+  //2. 小的在左，大的在右
+  var leader = arr[0];
+  var left = [];
+  var right = [];
+  for(var i = 1; i< arr.length; i ++){
+    if(arr[i] < leader) left.push(arr[i]);
+    else right.push(arr[i]);
   }
+  left = quickSort(left);
+  right = quickSort(right);
+  left.push(leader);
+  return left.concat(right);
 }
 
-sort(arr);
 
-console.log(arr);
+console.log(quickSort(arr));
+
 
