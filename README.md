1566ece806d471ed48749294afc6de8b657f9b5b
Lusaiwen <1811977648@qq.com>
2021-01-24 22:56:40 +0800

    6.标准快速排序

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # algorithm
 Lusaiwen/algorithm
 
-# 简单快速排序
+# 标准快速排序
 1. 选班长，小的在左，大的在右
 
 
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-
-var arr = [4,7,8,3,2,1,5,5,9,6];
-
-
-//简单快排，消耗性能，易于理解
-function quickSort(arr){
-  if(arr == null || arr.length == 0) return [];
-  //1. 选班长
-  //2. 小的在左，大的在右
-  var leader = arr[0];
-  var left = [];
-  var right = [];
-  for(var i = 1; i< arr.length; i ++){
-    if(arr[i] < leader) left.push(arr[i]);
-    else right.push(arr[i]);
-  }
-  left = quickSort(left);
-  right = quickSort(right);
-  left.push(leader);
-  return left.concat(right);
+function Node(value){
+  this.value = value;
+  this.next = null;
 }
 
+var node1 = new Node(1);
+var node2 = new Node(2);
+var node3 = new Node(3);
+var node4 = new Node(4);
+var node5 = new Node(5);
 
-console.log(quickSort(arr));
 
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
 
+//递归线性之链表
+function showNode(root){
+  if(root == null) return;
+  console.log(root.value);
+  showNode(root.next);
+}
+
+showNode(node1);
