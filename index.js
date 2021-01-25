b80135cc0f8a72dc04496f0aee3158771e74ace2
Lusaiwen <1811977648@qq.com>
2021-01-25 20:49:03 +0800

    6.标准快速排序

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-function Node(value){
-  this.value = value;
-  this.next = null;
+var arr = [4, 1, 6, 3, 2, 5, 7, 8, 9];
+
+
+
+function exchange(arr, a, b) {
+  var temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+function quickSort(arr, begin, end) {
+  if (begin >= end) return arr;
+  var left = begin;
+  var right = end;
+  while (left < right) {
+    while (left < right && arr[right] > arr[begin]) right--;
+    while (left < right && arr[left] <= arr[begin]) left++;
+    
+    if (left < right) {
+      exchange(arr, left, right);
+    }
+  }
+  console.log(begin, end, left, right);
+  exchange(arr, begin, left);
+
+  quickSort(arr, begin, left - 1);
+  quickSort(arr, left + 1, end);
+  return arr;
 }
 
-var node1 = new Node(1);
-var node2 = new Node(2);
-var node3 = new Node(3);
-var node4 = new Node(4);
-var node5 = new Node(5);
-
-
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-
-//递归线性之链表
-function showNode(root){
-  if(root == null) return;
-  console.log(root.value);
-  showNode(root.next);
+function sort(arr) {
+  if (arr == null || arr.length == 0) return [];
+  return quickSort(arr, 0, arr.length - 1);
 }
 
-showNode(node1);
+console.log(sort(arr));
