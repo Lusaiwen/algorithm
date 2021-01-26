1e97364ea51b179ab685aa1927efb0a7de9c1bbb
Lusaiwen <1811977648@qq.com>
2021-01-26 16:42:39 +0800

    10.二叉树的遍历

--- index.js
+++ index.js
@@ -20,11 +20,27 @@ c.right = g;
 b.left = d;
 b.right = e;
 
-function bianTree(root){
+//前序遍历  当前   左子树   右子树
+function preBian(root){
   if(root == null) return;
   console.log(root.value);
-  bianTree(root.left);
-  bianTree(root.right);
+  preBian(root.left);
+  preBian(root.right);
 }
 
-bianTree(a);
+//中序遍历  左子树  当前   右子树
+function middleBian(root){
+  if(root == null) return;
+  middleBian(root.left);
+  console.log(root.value);
+  middleBian(root.right);
+}
+
+//后序遍历  左子树  右子树  当前
+function backBian(root) {
+  if(root == null) return;
+  backBian(root.left);
+  backBian(root.right);
+  console.log(root.value);
+}
+backBian(a);
