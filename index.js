66f6d09252c16d9f3c6cd9f95b45aa0b9e5b3d86
Lusaiwen <1811977648@qq.com>
2021-02-02 12:58:54 +0800

    30.动态规划之斐波那契数列

--- index.js
+++ index.js
@@ -1,44 +1,12 @@
 
-function Node(value) {
-  this.value = value;
-  this.neighbour = [];
+
+//0, 1, 1, 2, 3, 5, 8,
+
+function fibo (n) {
+  if(n <= 0) return -1;
+  if(n == 1) return 0;
+  if(n == 2) return 1;
+  return fibo(n - 1) + fibo (n - 2);
 }
 
-var a = new Node('a');
-var b = new Node('b');
-var c = new Node('c');
-var d = new Node('d');
-var e = new Node('e');
-var f = new Node('f');
-
-
-a.neighbour.push(b);
-a.neighbour.push(c);
-b.neighbour.push(a);
-b.neighbour.push(c);
-b.neighbour.push(d);
-c.neighbour.push(a);
-c.neighbour.push(b);
-c.neighbour.push(d);
-d.neighbour.push(b);
-d.neighbour.push(e);
-e.neighbour.push(d);
-
-
-
-function bfs(nodes, target, path) {
-  if(nodes == null || nodes.length == 0) return false;
-  var nextNodes = [];
-  for(var i = 0; i < nodes.length; i ++){
-    if(path.indexOf(nodes[i]) > -1) continue;
-    path.push(nodes[i]);
-    if(nodes[i].value == target) return true;
-    else nextNodes = nextNodes.concat(nodes[i].neighbour);
-  }
-  return bfs(nextNodes, target, path);
-}
-
-console.log(bfs([a], 'n', []));
-
-
-
+console.log(fibo(8));
