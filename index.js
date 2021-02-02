5d70cba208fa13acf8105a2955f3c67a4e7d61e7
Lusaiwen <1811977648@qq.com>
2021-02-02 13:45:17 +0800

    32.动态规划之变态青蛙跳台阶

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
 
-//0, 1, 1, 2, 3, 5, 8,
-
+//f(n) = f(n-1) + f(n-2) + f(n-3) + ... f(2) + f(1) + f(0)
 function jump (n) {
   if(n <= 0) return -1;
   if(n == 1) return 1;
   if(n == 2) return 2;
-  return jump(n - 1) + jump(n - 2);
+  var result = 0;
+  for(var i = 1; i < n; i ++){
+    result += jump(n - i);
+  }
+  return result + 1; //加1代表从0级台阶直接跳上去
 }
 
-console.log(jump(5));
+console.log(jump(4));
